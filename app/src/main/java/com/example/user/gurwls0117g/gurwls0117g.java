abe71ebe93b10f4ebef3827db185e7e5b620a3ee
user <gurwls0117@gmail.com>
2017-07-07 15:33:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/gurwls0117g/gurwls0117g.java
@@ -0,0 +1,14 @@
+
+.package com.example.user.gurwls0117g;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gurwls0117g extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gurwls0117g);
+    }
+}
